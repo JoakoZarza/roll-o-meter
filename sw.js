f353da3c9f408941dc5c1444aa6ee8957f34a883
JoakoZarza <joakozarza@gmail.com>
2026-02-26 21:29:35 -0300

    Versión inicial de Roll-O-Meter

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'roll-o-meter-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800;900&display=swap'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Event (Offline support)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
